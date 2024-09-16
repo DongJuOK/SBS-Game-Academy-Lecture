04f4e4028ae430ef0ebe2c13280f2e1edada41f0
DongJuOK <kcary0907@gmail.com>
2024-09-16 22:50:06 +0900

    (2024.9.16) 보강

--- Class20th (GetAsyncKeyState)/Source.c
+++ Class20th (GetAsyncKeyState)/Source.c
@@ -5,8 +5,40 @@
 
 #define SIZE 10
 
+void Shuffle(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int seed = rand() % size;
+
+		int temporary = array[seed];
+
+		array[seed] = array[i];
+
+		array[i] = temporary;
+	}
+}
+
 int main()
 {
+#pragma region 셔플 함수
+
+	//  srand(time(NULL));
+	//  
+	//  //  0   1   2   3   4   5   6   7   8   9
+	//  // [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]
+	//  int list[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	//  
+	//  int size = sizeof(list) / sizeof(int);
+	//  
+	//  Shuffle(list, size);
+	//  
+	//  for (int i = 0; i < size; i++)
+	//  {
+	//  	printf("list[%d]의 값 : %d\n", i, list[i]);
+	//  }
+
+#pragma endregion
 
 #pragma region 포인터 배열
 	//  const char * dialog[SIZE];
